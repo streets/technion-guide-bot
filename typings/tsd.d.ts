c0eb9420f60dab3cb0620ba9a0ca2bcaae50d516
Sergey Bolshchikov <sergey@bolshchikov.net>
2016-04-28 15:15:02 +0300

    chore(typings): adding typings

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,1 +1,5 @@
 /// <reference path="jasmine/jasmine.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="form-data/form-data.d.ts" />
+/// <reference path="request/request.d.ts" />
+/// <reference path="fb-messenger-platform/fb-messenger-platform.d.ts" />
